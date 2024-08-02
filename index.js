bc6f7c34dd47e366d21df22d92d30fc8e36eaf77
davidpacco <david.j.pacco@gmail.com>
2024-08-02 09:47:45 -0500

    Add exercise 8.4

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -109,7 +109,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.author) {
+        return books.filter(b => b.author === args.author)
+      }
+
+      return books
+    },
     allAuthors: () => authors,
   },
 }
